e0fb7b6d3f28ac20b25e1c84410f3c189c1f5acf
Taylor Scott <Taylor_Scott@baylor.edu>
2013-04-10 00:08:53 -0500

    Update license text

--- query_phamerator.py
+++ query_phamerator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#Copyright (C) <year> <copyright holders>
+#Copyright (C) 2013 Taylor Scott
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy of
 #this software and associated documentation files (the "Software"), to deal in 
